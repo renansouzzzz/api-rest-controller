12594a36a78ed4ab7b26f67487eb32c7a024f0ac
renansouzzzz <renansouza.oliv@outlook.com>
2023-03-16 17:43:41 -0300

    first stop mvc production

---
+++ api-rest-controller/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using api_rest_controller.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_rest_controller.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CustomerController : ControllerBase
+
+{ 
+    private static List<Customer> customers = new List<Customer>();
+
+    [HttpGet]
+    public List<Customer> GetCustomers()
+    {
+        return customers;
+    }
+
+    [HttpPost]
+    public void AddCustomers([FromBody]Customer customer)
+    {
+        customers.Add(customer);
+        
+    } 
+}
